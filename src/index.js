cf8e5e23b391eb7f9a4cd03dfdcbe44d2c27d38e
Mlkz <maho.mamulashvili@gmail.com>
2023-01-13 15:30:06 -0500

    fetch and render map with d3

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log("Hello World!");
+import Example from './scripts/example';
+import chart from './scripts/map';
+import * as topojson from "topojson";
+import map from './scripts/map';
+
+
+document.addEventListener('DOMContentLoaded', async () => {
+  const res = await fetch(`https://cdn.jsdelivr.net/npm/us-atlas@3/states-albers-10m.json`)
+  const mapJson = await res.json()
+  const foo = map(mapJson)
+
+  console.log(foo)
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,51 +1,44 @@
-// Generated using webpack-cli https://github.com/webpack/webpack-cli
-
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
-
-const isProduction = process.env.NODE_ENV == 'production';
-
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const config = {
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-    },
-    devServer: {
-        open: true,
-        host: 'localhost',
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './dist/index.html',
-        }),
-
-        // Add your plugins here
-        // Learn more about plugins from https://webpack.js.org/configuration/plugins/
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-                type: 'asset',
-            },
-
-            // Add your rules for custom modules here
-            // Learn more about loaders from https://webpack.js.org/loaders/
-        ],
-    },
+  entry: [
+    path.resolve(__dirname, 'src', 'index.js'),
+    path.resolve(__dirname, 'src', 'index.scss')
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'), // bundled file in dist/
+    filename: '[name].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/, // applies to js files
+        use: ['babel-loader'], // transpiles your js
+        exclude: /node_modules/, // don't transpile node modules
+      },
+      {
+        test: /\.s?[ac]ss$/, // applies to css/scss/sass files
+        use: [
+          MiniCssExtractPlugin.loader, // create bundled css file
+          {
+            loader: 'css-loader', // resolves @import statements
+            options: { url: false } // don't resolve url() statements
+          },
+          'sass-loader', // compiles sass to css
+        ]
+      }
+    ]
+  },
+  plugins: [new MiniCssExtractPlugin()]
 };
 
-module.exports = () => {
-    if (isProduction) {
-        config.mode = 'production';
+module.exports = (env, argv) => {
+  if (argv.mode === 'production') {
+    config.devtool = 'source-map';
+  } else {
+    config.devtool = 'eval-source-map';
+  }
 
-
-        config.plugins.push(new WorkboxWebpackPlugin.GenerateSW());
-
-    } else {
-        config.mode = 'development';
-    }
-    return config;
-};
+  return config;
+}
